b6ebbba22c0ba84961702c1d35ffc9b53ba1a489
Alflof <alfred.lofving@gmail.com>
2026-09-18 10:26:51 +0200

    - added printWinner() for better DRY(?) - added/removed comments - void Playturn -> static void playTurn

--- src/main/java/se/iths/alfred/AlfredsDiceGame/Game.java
+++ src/main/java/se/iths/alfred/AlfredsDiceGame/Game.java
@@ -4,38 +4,36 @@ public class Game {
 
     boolean keepRunning = true;
 
+    //Loops until players decide to quit
     public void playGame(Player player1, Player player2) {
         while (keepRunning) {
 
             playTurn(player1);
             playTurn(player2);
 
-            //Poängredovisning
             IO.println("\nThe game is over, and the winner is...");
             if (player1.getScore() > player2.getScore()) {
-                IO.println(player1.getFullName() + " with " + player1.getScore() + " points! Congratulations!");
-                IO.println(player2.getFullName() + ", your " + player2.getScore() + " points were just not enough this time.");
+                printWinner(player1, player2);
             } else if (player1.getScore() < player2.getScore()) {
-                IO.println(player2.getFullName() + " with " + player2.getScore() + " points! Congratulations!");
-                IO.println(player1.getFullName() + ", your " + player1.getScore() + " points were just not enough this time.");
+                printWinner(player2, player1);
             } else {
                 IO.println("Neither of you - It's a tie! You both scored " + player1.getScore() + ".");
             }
 
-            //Starta om eller avsluta
+            //Reset player scores and restart, or quit game
             String playAgain = IO.readln("\nPress Enter to play again, or enter \"Q\" to quit. ");
             if (playAgain.equalsIgnoreCase("Q")) {
                 IO.println("Thank you for playing Alfred's Dice Game! Welcome back any time.");
                 keepRunning = false;
             } else {
-                IO.println("\nGreat! Let's see who wins this time.");
+                IO.println("Great, let's see who wins this time!\n");
                 player1.resetScore();
                 player2.resetScore();
             }
         }
     }
 
-    void playTurn(Player player) {
+    static void playTurn(Player player) {
         IO.readln(player.getFullName() + ", press Enter to roll your first die.");
         int diceRoll = Dice.rollDice();
         player.addToScore(diceRoll);
@@ -43,5 +41,10 @@ public class Game {
         diceRoll = Dice.rollDice();
         player.addToScore(diceRoll);
         IO.println("You rolled a " + diceRoll + "!");
+    }
+
+    static void printWinner(Player winner, Player loser) {
+        IO.println("***** " + winner.getFullName() + " with " + winner.getScore() + " points! Congratulations! *****");
+        IO.println(loser.getFullName() + ", your " + loser.getScore() + " points were just not enough this time.");
     }
 }
